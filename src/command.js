3ece2317a6539b88c1aafa6d3bb1aca39cb77e30
Danylo Karpenko <fistashka365@gmail.com>
2019-09-27 14:32:43 +0300

    Second step

--- src/command.js
+++ src/command.js
@@ -3,6 +3,15 @@ const program = require('commander');
 export default () => {
   program
       .description('Compares two configuration files and shows a difference.')
-      .version('0.0.1', '-V, --version', 'output the current version')
-      .parse(process.argv);
+      .version('0.0.1', '-V, --version', 'output the current version');
+
+  program
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action(function(firstConfig, secondConfig, type) {
+      type = type || 'plain';
+      console.log(firstConfig);
+    });
+
+  program.parse(process.argv);
 };
