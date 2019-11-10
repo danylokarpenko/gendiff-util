4292a2af1fc65fae4e6debaedf49cc508e4881a3
Danylo Karpenko <fistashka365@gmail.com>
2019-11-10 17:29:32 +0200

    removed reading file from parse module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 import parse from './parsers';
 import buildAst from './astBuilder';
 import getFormater from './formatters';
 
 export default (configPath1, configPath2, format) => {
-  const data1 = parse(configPath1);
-  const data2 = parse(configPath2);
+  const fileData1 = fs.readFileSync(configPath1, 'utf-8');
+  const fileData2 = fs.readFileSync(configPath2, 'utf-8');
+
+  const fileFormat1 = path.extname(configPath1).slice(1);
+  const fileFormat2 = path.extname(configPath2).slice(1);
+
+  const data1 = parse(fileData1, fileFormat1);
+  const data2 = parse(fileData2, fileFormat2);
 
   const ast = buildAst(data1, data2);
   const render = getFormater(format);
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,5 @@
-import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from 'ini';
-import path from 'path';
 
 const parsers = {
   json: JSON.parse,
@@ -9,13 +7,4 @@ const parsers = {
   ini: ini.parse,
 };
 
-const getParsedData = (data, type) => parsers[type](data);
-
-export default (configPath) => {
-  const data = fs.readFileSync(configPath, 'utf-8');
-  const dataType = path.extname(configPath).slice(1);
-
-  const result = getParsedData(data, dataType);
-
-  return result;
-};
+export default (data, type) => parsers[type](data);
